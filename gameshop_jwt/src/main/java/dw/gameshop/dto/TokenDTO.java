3699a8dedeee92f096a8385585f106c9b8129cd5
gilsu44145942 <asdf441467@gmail.com>
2025-03-24 17:12:07 +0900

    03/24

---
+++ gameshop_jwt/src/main/java/dw/gameshop/dto/TokenDTO.java
@@ -0,0 +1,12 @@
+package dw.gameshop.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class TokenDTO {
+    private String token;
+
+}
---
+++ gameshop_jwt/src/main/java/dw/gameshop/service/UserDetailService.java
@@ -0,0 +1,27 @@
+package dw.gameshop.service;
+
+import dw.gameshop.model.User;
+import dw.gameshop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findById((username));
+        if(user.isEmpty()){
+            throw new IllegalAccessError(username);
+        }
+        return user.get();
+    }
+}
